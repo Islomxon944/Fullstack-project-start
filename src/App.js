79a152fd6e6cf6ad02597519c8ec7dbaf1c69b21
Islomxon944 <narzullayev0507@gmail.com>
2023-12-12 11:42:57 +0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom"
-import {Main, Login, Register} from './components'
+import {Navbar, Main, Login, Register,} from './components'
 
 const App = () => {
   return (
+      <div>
+    <Navbar />
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
+      </div>
   )
 }
 
